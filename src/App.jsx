db85cb56d822e1b84619466cf0c1a1c6c3cd7b07
Nicholas Lee <nicholasleexyz@gmail.com>
2023-09-08 20:00:35 -0600

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className="content-wrapper">
+      <div className="content">
+        <button className="grid-item">Create</button>
+        <button className="grid-item">Read</button>
+        <button className="grid-item">Update</button>
+        <button className="grid-item">Delete</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
