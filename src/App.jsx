b1db74ce1d7ace5663d94871be7e6b7c279d9a1c
Nicholas Lee <nicholasleexyz@gmail.com>
2023-09-14 18:16:37 -0600

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,111 +1,137 @@
-import { useEffect, useRef, useState } from "react";
 import Entry from "./Entry";
+// import { useEffect, useRef, useState } from "react";
 
-const endpoint = "https://64d5c8e3613ee4426d9799bd.mockapi.io/promineo/users";
-const data = await fetch(endpoint).then((response) => response.json());
+// const endpoint = "https://64d5c8e3613ee4426d9799bd.mockapi.io/promineo/users";
+// const data = await fetch(endpoint).then((response) => response.json());
 
-// useEffect(() => {
-//   fetch(
-//     `${baseUrl}apikey=${apikey}&i=${movies.Search[currentMovieIndex].imdbID}`
-//   )
-//     .then((response) => response.json())
-//     .then((data) => setCurrentMovieData(data));
-// }, [currentMovieIndex]);
-
-function App() {
-  const [currentData, setCurrentData] = useState(data);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [post, setPost] = useState(false);
-
-  const inputUser = useRef(null);
-  const inputFullName = useRef(null);
-  const inputEmail = useRef(null);
-
-  const f = async () =>
-    await fetch(endpoint)
-      .then((response) => response.json())
-      .then((data) => {
-        setCurrentData(data);
-        setIsLoaded(false);
-      });
-
-  useEffect(() => {
-    if (post) {
-      const postFunc = async () => {
-        const body = JSON.stringify({
-          user: inputUser.current.value,
-          name: inputFullName.current.value,
-          email: inputEmail.current.value,
-        });
-
-        const requestOptions = {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: body,
-        };
-
-        await fetch(
-          "https://64d5c8e3613ee4426d9799bd.mockapi.io/promineo/users/",
-          requestOptions
-        );
-
-        inputUser.current.value = "";
-        inputFullName.current.value = "";
-        inputEmail.current.value = "";
-      };
-
-      postFunc();
-      setPost(false);
-      setIsLoaded(true);
-    }
-  }, [post]);
-
-  useEffect(() => {
-    // console.log(currentData);
-    f();
-  }, [isLoaded]);
-
+export default function App() {
   return (
     <div className="content">
-      <div className="new-entry-wrapper">
-        <div className="new-entry">
-          <div className="iput">
-            <label className="" htmlFor="">
-              User:{" "}
-            </label>
-            <input className="" type="text" name="" id="" ref={inputUser} />
-          </div>
-          <div className="iput">
-            <label className="" htmlFor="">
-              Full Name:{" "}
-            </label>
-            <input className="" type="text" name="" id="" ref={inputFullName} />
-          </div>
-          <div className="iput">
-            <label className="" htmlFor="">
-              Email:{" "}
-            </label>
-            <input className="" type="text" name="" id="" ref={inputEmail} />
-          </div>
-          <button onClick={() => setPost(true)} className="btn btn-new">
-            New
-          </button>
-        </div>
+      <div className="content-info">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
+          dolorum laboriosam, rerum saepe aliquid sit nihil. Sit assumenda quos
+          eum eaque! Vel distinctio quidem, officiis fuga optio ea eaque omnis!
+        </p>
+        <span>Lorem, ipsum dolor.</span>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          provident tempore eveniet dolor reprehenderit harum officia ducimus
+          unde, aut non recusandae ad. Repudiandae cumque voluptas blanditiis
+          excepturi! Autem aspernatur error nulla soluta sapiente adipisci
+          consequatur eius corrupti at ut voluptatem vero, facilis enim possimus
+          earum asperiores pariatur ipsa a porro! Molestias qui quaerat
+          excepturi consequatur nulla eaque, voluptates delectus, corporis
+          cumque fugiat tempore perspiciatis reprehenderit incidunt aspernatur
+          illo nostrum?
+        </p>
       </div>
-      <div className="entry-container">
-        {currentData.map((d, i) => (
-          <Entry
-            key={i}
-            user={d.user}
-            email={d.email}
-            fullName={d.name}
-            imgAvatar={d.avatar}
-            index={d.id}
-          ></Entry>
-        ))}
+      <div className="content-entries">
+        <Entry></Entry>
+        <Entry></Entry>
+        <Entry></Entry>
+        <Entry></Entry>
+        <Entry></Entry>
+        <Entry></Entry>
+        <Entry></Entry>
+        <Entry></Entry>
       </div>
     </div>
   );
 }
+// const [currentData, setCurrentData] = useState(data);
+// const [isLoaded, setIsLoaded] = useState(false);
 
-export default App;
+// const inputUser = useRef(null);
+// const inputFullName = useRef(null);
+// const inputEmail = useRef(null);
+
+// const controller = new AbortController();
+
+// function post() {
+//   const postFunc = async () => {
+//     const body = JSON.stringify({
+//       user: inputUser.current.value,
+//       name: inputFullName.current.value,
+//       email: inputEmail.current.value,
+//     });
+
+//     const requestOptions = {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: body,
+//       signal: controller.signal,
+//     };
+
+//     await fetch(
+//       "https://64d5c8e3613ee4426d9799bd.mockapi.io/promineo/users/",
+//       requestOptions
+//     );
+
+//     inputUser.current.value = "";
+//     inputFullName.current.value = "";
+//     inputEmail.current.value = "";
+
+//     setIsLoaded(false);
+//   };
+
+//   postFunc();
+
+//   return () => {
+//     controller.abort();
+//   };
+// }
+
+// useEffect(() => {
+//   const f = async () =>
+//     await fetch(endpoint)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setCurrentData(data);
+//         setIsLoaded(true);
+//       });
+
+//   f();
+// }, [isLoaded]);
+
+// return (
+//   <div className="content">
+//     <div className="new-entry-wrapper">
+//       <div className="new-entry">
+//         <div className="iput">
+//           <label className="" htmlFor="">
+//             User:{" "}
+//           </label>
+//           <input className="" type="text" name="" id="" ref={inputUser} />
+//         </div>
+//         <div className="iput">
+//           <label className="" htmlFor="">
+//             Full Name:{" "}
+//           </label>
+//           <input className="" type="text" name="" id="" ref={inputFullName} />
+//         </div>
+//         <div className="iput">
+//           <label className="" htmlFor="">
+//             Email:{" "}
+//           </label>
+//           <input className="" type="text" name="" id="" ref={inputEmail} />
+//         </div>
+//         <button onClick={post} className="btn btn-new">
+//           New
+//         </button>
+//       </div>
+//     </div>
+//     <div className="entry-container">
+//       {currentData.map((d, i) => (
+//         <Entry
+//           key={i}
+//           user={d.user}
+//           email={d.email}
+//           fullName={d.name}
+//           imgAvatar={d.avatar}
+//           index={d.id}
+//         ></Entry>
+//       ))}
+//     </div>
+//   </div>
+// );
--- src/Entry.jsx
+++ src/Entry.jsx
@@ -1,82 +1,70 @@
 import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-function Entry({ user, email, fullName, imgAvatar, index }) {
-  const [del, setDel] = useState(false);
-  const [edit, setEdit] = useState(false);
-  const [idx, setIndex] = useState(index);
-
-  const [currentUser, setCurrentUser] = useState(user);
-  const [currentEmail, setCurrentEmail] = useState(email);
-  const [currentFullName, setCurrentFullName] = useState(fullName);
-
-  useEffect(() => {
-    if (del === true) {
-      console.log("deleting at index: " + idx);
-      fetch(
-        "https://64d5c8e3613ee4426d9799bd.mockapi.io/promineo/users/" + idx,
-        {
-          method: "DELETE",
-        }
-      );
-    } else if (edit === true) {
-      console.log("Editing at index: " + idx);
-
-      const desiredUser = "asdf";
-      const desiredFullName = "jkl";
-      const desiredEmail = "qwerty";
-
-      setCurrentUser(desiredUser);
-      setCurrentFullName(desiredFullName);
-      setCurrentEmail(desiredEmail);
-
-      const body = JSON.stringify({
-        user: desiredUser,
-        name: desiredFullName,
-        email: desiredEmail,
-      });
-
-      const requestOptions = {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: body,
-      };
-
-      const updateData = async () => {
-        await fetch(
-          "https://64d5c8e3613ee4426d9799bd.mockapi.io/promineo/users/" + idx,
-          requestOptions
-        )
-          .then((res) => res.json())
-          .then((dat) => setIndex(dat.id));
-
-        setEdit(false);
-      };
-
-      updateData();
-    }
-  }, [del, edit, idx, currentEmail, currentFullName, currentUser]);
-
-  return (
-    <div className={`entry ${del ? "hidden" : ""}`}>
-      <img src={imgAvatar}></img>
-      <span>User: {currentUser}</span>
-      <span>Full Name: {currentFullName}</span>
-      <span>Email: {currentEmail}</span>
-
-      <button className="btn-edit" onClick={() => setEdit(true)}>
-        Edit
-      </button>
-      <button className="btn-delete" onClick={() => setDel(true)}>
-        Delete
-      </button>
-      {/* 
-        edit button (update)
-        delete button (╯°□°）╯︵ ┻━┻
-       */}
-    </div>
-  );
+// export default function Entry({ user, email, fullName, imgAvatar, index }) {
+export default function Entry() {
+  return <div className="entry">asdf</div>;
 }
+
+// const [del, setDel] = useState(false);
+// const [edit, setEdit] = useState(false);
+// const [idx, setIndex] = useState(index);
+// const [currentUser, setCurrentUser] = useState(user);
+// const [currentEmail, setCurrentEmail] = useState(email);
+// const [currentFullName, setCurrentFullName] = useState(fullName);
+// useEffect(() => {
+//   if (del === true) {
+//     console.log("deleting at index: " + idx);
+//     fetch(
+//       "https://64d5c8e3613ee4426d9799bd.mockapi.io/promineo/users/" + idx,
+//       {
+//         method: "DELETE",
+//       }
+//     );
+//   } else if (edit === true) {
+//     console.log("Editing at index: " + idx);
+//     const desiredUser = "asdf";
+//     const desiredFullName = "jkl";
+//     const desiredEmail = "qwerty";
+//     setCurrentUser(desiredUser);
+//     setCurrentFullName(desiredFullName);
+//     setCurrentEmail(desiredEmail);
+//     const body = JSON.stringify({
+//       user: desiredUser,
+//       name: desiredFullName,
+//       email: desiredEmail,
+//     });
+//     const requestOptions = {
+//       method: "PUT",
+//       headers: { "Content-Type": "application/json" },
+//       body: body,
+//     };
+//     const updateData = async () => {
+//       await fetch(
+//         "https://64d5c8e3613ee4426d9799bd.mockapi.io/promineo/users/" + idx,
+//         requestOptions
+//       )
+//         .then((res) => res.json())
+//         .then((dat) => setIndex(dat.id));
+//       setEdit(false);
+//     };
+//     updateData();
+//   }
+// }, [del, edit, idx, currentEmail, currentFullName, currentUser]);
+// return (
+//   <div className={`entry ${del ? "hidden" : ""}`}>
+//     <img src={imgAvatar}></img>
+//     <span>User: {currentUser}</span>
+//     <span>Full Name: {currentFullName}</span>
+//     <span>Email: {currentEmail}</span>
+//     <button className="btn-edit" onClick={() => setEdit(true)}>
+//       Edit
+//     </button>
+//     <button className="btn-delete" onClick={() => setDel(true)}>
+//       Delete
+//     </button>
+//   </div>
+// );
 
 // :P
 Entry.propTypes = {
@@ -86,5 +74,3 @@ Entry.propTypes = {
   imgAvatar: PropTypes.string,
   index: PropTypes.string,
 };
-
-export default Entry;
