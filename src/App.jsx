11c068af46ac92a3947db04903e55666e7d55b3d
Nicholas Lee <nicholasleexyz@gmail.com>
2023-09-15 20:15:33 -0600

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,30 @@ import ContentEntries from "./ContentEntries";
 export default function App() {
   const [userData, setUserData] = useState([]);
 
+  //READ
   async function getUserData() {
     const endpoint =
       "https://64d5c8e3613ee4426d9799bd.mockapi.io/promineo/users";
+
     const userDat = await fetch(endpoint)
       .then((res) => res.json())
-      .then((req) => req);
+      .then((dat) => dat);
+
     setUserData(userDat);
   }
+
+  //DELETE
+  //EDIT (PUT)
+  //CREATE (POST)
 
   useEffect(() => {
     getUserData();
     return () => {};
   }, []);
 
-  useEffect(() => {
-    console.log(userData);
-  }, [userData]);
+  // useEffect(() => {
+  //   console.log(userData);
+  // }, [userData]);
 
   return (
     <div className="content">
